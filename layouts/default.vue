29bc132a5c0e4700f2e604bf9a19f8e5dbd9dbe7
deozza <deozza@gmail.com>
2021-10-13 18:38:10 +0200

    added dropdown menu for species

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,13 @@
     <div class="vertical-menu">
       <div class="flex-column flex-left">
         <a href="/">Accueil</a>
-        <a href="/species">Espèces</a>
+        <button @click="showDropdown = !showDropdown">Espèces</button>
+        <div v-if="showDropdown === true" class="flex-column flex-left dropdown" >
+          <a href="/species">Voir toutes</a>
+          <a href="/species/fish">Poissons</a>
+          <a href="/species/plant">Plantes</a>
+          <a href="/species/invertebrate">Invertébrés</a>
+        </div>
         <a href="/users">Utilisateurs</a>
         <a href="/aquariums">Aquariums</a>
       </div>
@@ -19,6 +25,9 @@
 import Vue from 'vue'
 
 export default Vue.extend({
+  data() {
+    return { showDropdown: false}
+  },
   methods: {
     async logout(){
       this.$cookies.remove('appquarium-jwt')
@@ -42,14 +51,22 @@ export default Vue.extend({
    box-shadow: 10px 0 5px grey;
  }
 
- div.container > div.vertical-menu > div.flex-column > a{
+ div.container > div.vertical-menu > div.flex-column > a,
+ div.container > div.vertical-menu > div.flex-column > button{
    color: black;
    font-size: 28px;
    padding: 36px 0 36px 68px;
    text-decoration: none;
  }
 
- div.container > div.vertical-menu > a{
+ div.container > div.vertical-menu > div.flex-column > button {
+   background-color: transparent;
+   border: none;
+   cursor: pointer;
+ }
+
+ div.container > div.vertical-menu > a,
+ div.container > div.vertical-menu > button{
    color: black;
    font-size: 28px;
    padding: 0 0 36px 68px;
@@ -59,7 +76,18 @@ export default Vue.extend({
    cursor: pointer;
  }
 
+ div.container > div.vertical-menu > div.flex-column > div.dropdown > a {
+   color: black;
+   font-size: 18px;
+   padding: 0 0 24px 94px;
+   text-decoration: none;
+   bottom: 0;
+   cursor: pointer;
+ }
+
  div.container > div.vertical-menu > div.flex-column > a:hover,
+ div.container > div.vertical-menu > div.flex-column > button:hover,
+ div.container > div.vertical-menu > div.flex-column > div.dropdown > a:hover,
  div.container > div.vertical-menu > a:hover {
    color: gray;
  }
