dcb41ff300947fd985a3c0993d9e696534476a4e
deozza <deozza@gmail.com>
2021-10-13 15:23:36 +0200

    removed BaseStats unused molecule

--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,7 @@
 <main>
   <BaseHeader :base-header-model="header"/>
 
-  <div id="loading" v-if="$fetchState.pending">
-    <p >Récupération des infos️</p>
-  </div>
-  <div id="error" v-else-if="$fetchState.error">
-    <p >Une erreur est survenue :(</p>
-  </div>
-  <div id="content" v-else>
+  <div id="content">
     <section id="users">
       <BaseHeader :base-header-model="userStatsHeader" />
     </section>
@@ -26,19 +20,13 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import BaseStatsModel from "~/components/molecules/pages/index/stats/BaseStatsModel";
-import BaseStats from "~/components/molecules/pages/index/stats/BaseStats.vue";
 import BaseHeaderModel from "~/components/atoms/typography/header/BaseHeaderModel";
 import BaseHeader from "~/components/atoms/typography/header/BaseHeader.vue";
-import Result from "~/app/utils/useCasesResult/Result";
-import SpeciesUseCase from "~/app/species/global/useCases/UseCase";
-import UserUseCase from "~/app/user/useCases/UseCase";
 
 export default Vue.extend({
   middleware: 'authenticated',
   components: {
-    BaseHeader,
-    BaseStats
+    BaseHeader
   },
   data(){
     const header: BaseHeaderModel = new BaseHeaderModel('Accueil')
