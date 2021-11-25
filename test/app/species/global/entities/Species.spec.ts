0693d5b23223e206219cb3a7bb16ffbb0fb57e3a
deozza <deozza@gmail.com>
2021-11-25 11:37:57 +0100

    tests for Species.ts entity functions

---
+++ test/app/species/global/entities/Species.spec.ts
@@ -0,0 +1,112 @@
+import Species from "~/app/species/global/entities/Species";
+
+let species: Species
+
+describe('Testing Species.ts entity', () => {
+
+  describe('computeLinkToSpecies', () => {
+    test('with valid data', () => {
+      species = new Species([])
+      species.category = "category"
+      species.uuid = "uuid"
+
+      expect(species.computeLinkToSpecies()).toEqual('/species/category/uuid')
+    })
+
+    test('without data would fail', () => {
+      species = new Species([])
+
+      expect(()=>species.computeLinkToSpecies()).toThrow(new Error('Invalid species object'))
+    })
+
+    test('without category would fail', () => {
+      species = new Species([])
+      species.uuid = 'uuid'
+
+      expect(()=>species.computeLinkToSpecies()).toThrow(new Error('Invalid species object'))
+    })
+
+    test('without uuid would fail', () => {
+      species = new Species([])
+      species.category = 'category'
+
+      expect(()=>species.computeLinkToSpecies()).toThrow(new Error('Invalid species object'))
+    })
+  })
+
+  describe('computeName', () => {
+    test('with valid data', () => {
+      species = new Species([])
+      species.species_naming.species_genre.name = "Genrename"
+      species.species_naming.name = "Speciesname"
+
+      expect(species.computeName()).toEqual('Genrename Speciesname')
+    })
+
+    test('without data would fail', () => {
+      species = new Species([])
+
+      expect(species.computeName()).toEqual('NA')
+    })
+
+    test('without species_genre would fail', () => {
+      species = new Species([])
+      species.species_naming.name = 'Speciesname'
+
+      expect(species.computeName()).toEqual('NA')
+    })
+
+    test('without species_naming.name would fail', () => {
+      species = new Species([])
+      species.species_naming.species_genre.name = 'Genrename'
+
+      expect(species.computeName()).toEqual('NA')
+    })
+  })
+
+  describe('getPublicationStateStyle', () => {
+    test('with valid data', () => {
+      species = new Species([])
+      species.publication_state = "DRAFT"
+
+      expect(species.getPublicationStateStyle()).toEqual('secondary')
+    })
+
+    test('without data would fail', () => {
+      species = new Species([])
+
+      expect(()=>species.getPublicationStateStyle()).toThrow(new Error('Invalid species object'))
+    })
+
+    test('without valid data would fail', () => {
+      species = new Species([])
+      species.publication_state = 'invalid'
+
+      expect(()=>species.getPublicationStateStyle()).toThrow(new Error('Unexpected publication_state : invalid'))
+    })
+  })
+
+  describe('getPublicationStateContent', () => {
+    test('with valid data', () => {
+      species = new Species([])
+      species.publication_state = "DRAFT"
+
+      expect(species.getPublicationStateContent()).toEqual('brouillon')
+    })
+
+    test('without data would fail', () => {
+      species = new Species([])
+
+      expect(()=>species.getPublicationStateContent()).toThrow(new Error('Invalid species object'))
+    })
+
+    test('without valid data would fail', () => {
+      species = new Species([])
+      species.publication_state = 'invalid'
+
+      expect(()=>species.getPublicationStateContent()).toThrow(new Error('Unexpected publication_state : invalid'))
+    })
+  })
+
+
+})
