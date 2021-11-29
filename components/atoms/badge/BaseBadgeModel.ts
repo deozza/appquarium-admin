9528e384cb604dad0d156dbbcfb4aa1f2da8a344
deozza <deozza@gmail.com>
2021-11-29 15:39:55 +0100

    BaseBadgeModel is tested and code has been adapted

--- components/atoms/badge/BaseBadgeModel.ts
+++ components/atoms/badge/BaseBadgeModel.ts
@@ -1,53 +1,61 @@
 export default class BaseBadgeModel {
-    content: string | any
-    style: string
-    size: string
-    icon: string
-    isOnlyIcon: boolean
 
-    constructor(
-        content: string | any,
-        style: string = 'primary',
-        size: string = 'normal',
-        icon: string = '',
-        isOnlyIcon: boolean = false,
-    ) {
-        this.content = content
+  readonly EXPECTED_STYLES: Array<string> = [
+    'primary',
+    'secondary',
+    'success',
+    'danger',
+    'warning',
+    'info',
+    'link'
+  ]
 
-        const EXPECTED_STYLES: Array<string> = [
-            'primary',
-            'secondary',
-            'success',
-            'danger',
-            'warning',
-            'info',
-            'link'
-        ]
-        if(EXPECTED_STYLES.includes(style) === false){
-            throw Error("Style '"+style+"' is not a valid style for BaseBadgeModel")
-        }
-        this.style = style
+  readonly DEFAULT_STYLE: string = this.EXPECTED_STYLES[0]
 
-        const EXPECTED_SIZES: Array<string> = [
-            'small',
-            'normal',
-            'large',
-        ]
-        if(EXPECTED_SIZES.includes(size) === false){
-            throw Error("Size '"+size+"' is not a valid size for BaseBadgeModel")
-        }
-        this.size = size
+  readonly EXPECTED_SIZES: Array<string> = [
+    'normal',
+    'small',
+    'large',
+  ]
 
+  readonly DEFAULT_SIZE: string = this.EXPECTED_SIZES[0]
 
-        this.icon = icon
+  content: string | any
+  style: string
+  size: string
+  icon: string | null
+  isOnlyIcon: boolean | null
 
-        if(this.icon !== '' && isOnlyIcon === true){
-            throw Error("If no icon is set for BaseBadgeModel, the value for 'isOnlyIcon' can not be 'true'")
-        }
-        this.isOnlyIcon = isOnlyIcon
-    }
+  constructor(content: string | any) {
+    this.content = content
+    this.size = this.DEFAULT_SIZE
+    this.style = this.DEFAULT_STYLE
+    this.icon = null
+    this.isOnlyIcon = null
+  }
 
   toJSON () {
     return { ...this } // here I make a POJO's copy of the class instance
   }
+
+  public setStyleOrThrowError(style: string): void {
+    if(this.EXPECTED_STYLES.includes(style) === false){
+      throw Error("Style '"+style+"' is not a valid style for BaseBadgeModel")
+    }
+    this.style = style
+  }
+
+  public setSizeOrThrowError(size: string): void {
+    if(this.EXPECTED_SIZES.includes(size) === false){
+      throw Error("Size '"+size+"' is not a valid size for BaseBadgeModel")
+    }
+    this.size = size
+  }
+
+  public setIconOrThrowError(icon: string, isOnlyIcon: boolean = false): void {
+    this.icon = icon
+    this.isOnlyIcon = isOnlyIcon
+  }
+
+
 }
---
+++ test/components/atoms/badge/BaseBadgeModel.spec.ts
@@ -0,0 +1,39 @@
+import BaseBadgeModel from "~/components/atoms/badge/BaseBadgeModel";
+
+describe('Testing BaseBadgeModel.ts component', () => {
+
+  test('customize with valid style', () => {
+    let badge: BaseBadgeModel = new BaseBadgeModel('content')
+    expect(badge.style).toEqual('primary')
+    badge.setStyleOrThrowError('secondary')
+    expect(badge.style).toEqual('secondary')
+  })
+
+  test('unexpected style throw error', () => {
+    let badge: BaseBadgeModel = new BaseBadgeModel('content')
+
+    expect(() => badge.setStyleOrThrowError('unexpectedStyle')).toThrowError(Error("Style 'unexpectedStyle' is not a valid style for BaseBadgeModel"))
+  })
+
+  test('customize with valid size', () => {
+    let badge: BaseBadgeModel = new BaseBadgeModel('content')
+    expect(badge.size).toEqual('normal')
+    badge.setSizeOrThrowError('small')
+    expect(badge.size).toEqual('small')
+  })
+
+  test('unexpected size throw error', () => {
+    let badge: BaseBadgeModel = new BaseBadgeModel('content')
+    expect(() => badge.setSizeOrThrowError('unexpectedSize')).toThrowError(Error("Size 'unexpectedSize' is not a valid size for BaseBadgeModel"))
+  })
+
+  test('customize with icon', () => {
+    let badge: BaseBadgeModel = new BaseBadgeModel('content')
+    expect(badge.icon).toBeNull()
+    expect(badge.isOnlyIcon).toBeNull()
+    badge.setIconOrThrowError('icon')
+    expect(badge.icon).toEqual('icon')
+    expect(badge.isOnlyIcon).toBeFalsy()
+  })
+
+})
