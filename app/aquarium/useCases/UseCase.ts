e63b8f45ff382c4dd9006cad4211286d2ab75391
deozza <deozza@gmail.com>
2021-12-02 23:02:04 +0100

    removed dead code

--- app/aquarium/useCases/UseCase.ts
+++
@@ -1,6 +0,0 @@
-import UseCaseInterface from "~/app/aquarium/useCases/UseCaseInterface";
-
-export default class AquariumUseCase implements UseCaseInterface{
-
-
-}
--- app/aquarium/useCases/UseCaseInterface.ts
+++
@@ -1,4 +0,0 @@
-import Result from "~/app/utils/useCasesResult/Result";
-
-export default interface UseCaseInterface {
-}
