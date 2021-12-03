0ec9a478208f4dcde45eeeeeb40e1e0858fd80d6
deozza <deozza@gmail.com>
2021-12-03 01:21:28 +0100

    written some tests for global species UseCase.ts

---
+++ test/app/species/global/useCases/UseCase.spec.ts
@@ -0,0 +1,203 @@
+import SpeciesUseCase from "~/app/species/global/useCases/UseCase";
+import Services from "~/app/species/global/services/Services";
+import Result from "~/app/utils/useCasesResult/Result";
+import UseCaseSuccess from "~/app/utils/useCasesResult/types/UseCaseSuccess";
+import UseCaseError from "~/app/utils/useCasesResult/types/UseCaseError";
+import Species from "~/app/species/global/entities/Species";
+import User from "~/app/user/entities/User";
+
+jest.mock("~/app/species/global/services/Services")
+
+describe('Testing SpeciesUseCase.ts', () => {
+
+  test('getTotalSpecies returns success on Services success', async () => {
+
+    Services.prototype.queryTotalSpecies = jest.fn().mockImplementationOnce(() => {
+      return 1;
+    });
+
+    const useCase: SpeciesUseCase = new SpeciesUseCase()
+
+    const expectedResponse: Result = new Result()
+    expectedResponse.content = 1
+    expectedResponse.success = new UseCaseSuccess('Query is ok', 200)
+
+    expect(await useCase.getTotalSpecies('jwt')).toEqual(expectedResponse)
+  })
+
+  test('getTotalSpecies returns error on null', async () => {
+
+    Services.prototype.queryTotalSpecies = jest.fn().mockImplementationOnce(() => {
+      return null;
+    });
+
+    const useCase: SpeciesUseCase = new SpeciesUseCase()
+
+    const expectedResponse: Result = new Result()
+    expectedResponse.errors = [new UseCaseError('Query failed', 400)]
+
+    expect(await useCase.getTotalSpecies('jwt')).toEqual(expectedResponse)
+  })
+
+  test('getSpecies returns success on Services success', async () => {
+
+    Services.prototype.queryGetSpecies = jest.fn().mockImplementationOnce(() => {
+      return new Species([]);
+    });
+
+    const useCase: SpeciesUseCase = new SpeciesUseCase()
+
+    const expectedResponse: Result = new Result()
+    expectedResponse.content = new Species([])
+    expectedResponse.success = new UseCaseSuccess('Query is ok', 200)
+    const response: Result = await useCase.getSpecies('jwt', 'uuid')
+
+
+    expect(response.isSuccessful()).toBeTruthy()
+  })
+
+  test('getSpecies returns error on Services failure error', async () => {
+
+    Services.prototype.queryGetSpecies = jest.fn().mockImplementationOnce(() => {
+      return new UseCaseError('error', 400);
+    });
+
+    const useCase: SpeciesUseCase = new SpeciesUseCase()
+
+    const expectedResponse: Result = new Result()
+    expectedResponse.errors = [new UseCaseError('Query failed', 400)]
+
+    expect(await useCase.getSpecies('jwt', 'uuid')).toEqual(expectedResponse)
+  })
+
+  test('getSpecies returns error on Services failure not found', async () => {
+
+    Services.prototype.queryGetSpecies = jest.fn().mockImplementationOnce(() => {
+      return new UseCaseError('error', 404);
+    });
+
+    const useCase: SpeciesUseCase = new SpeciesUseCase()
+
+    const expectedResponse: Result = new Result()
+    expectedResponse.errors = [new UseCaseError('Species not found', 404)]
+
+    expect(await useCase.getSpecies('jwt', 'uuid')).toEqual(expectedResponse)
+  })
+
+  test('getListOfSpecies returns success on Services success', async () => {
+
+    Services.prototype.queryListOfSpecies = jest.fn().mockImplementationOnce(() => {
+      return [];
+    });
+
+    const useCase: SpeciesUseCase = new SpeciesUseCase()
+
+    const expectedResponse: Result = new Result()
+    expectedResponse.content = []
+    expectedResponse.success = new UseCaseSuccess('Query is ok', 200)
+
+    expect(await useCase.getListOfSpecies('jwt')).toEqual(expectedResponse)
+  })
+
+  test('getListOfSpecies returns error on null', async () => {
+
+    Services.prototype.queryListOfSpecies = jest.fn().mockImplementationOnce(() => {
+      return new UseCaseError('error', 400);
+    });
+
+    const useCase: SpeciesUseCase = new SpeciesUseCase()
+
+    const expectedResponse: Result = new Result()
+    expectedResponse.errors = [new UseCaseError('error', 400)]
+
+    expect(await useCase.getListOfSpecies('jwt')).toEqual(expectedResponse)
+  })
+
+
+  test('getSpeciesCategories returns success on Services success', async () => {
+
+    Services.prototype.querySpeciesCategories = jest.fn().mockImplementationOnce(() => {
+      return [];
+    });
+
+    const useCase: SpeciesUseCase = new SpeciesUseCase()
+
+    const expectedResponse: Result = new Result()
+    expectedResponse.content = []
+    expectedResponse.success = new UseCaseSuccess('Query is ok', 200)
+
+    expect(await useCase.getSpeciesCategories('jwt')).toEqual(expectedResponse)
+  })
+
+  test('getSpeciesCategories returns error on null', async () => {
+
+    Services.prototype.querySpeciesCategories = jest.fn().mockImplementationOnce(() => {
+      return new UseCaseError('error', 400);
+    });
+
+    const useCase: SpeciesUseCase = new SpeciesUseCase()
+
+    const expectedResponse: Result = new Result()
+    expectedResponse.errors = [new UseCaseError('error', 400)]
+
+    expect(await useCase.getSpeciesCategories('jwt')).toEqual(expectedResponse)
+  })
+
+  test('getSpeciesOrigins returns success on Services success', async () => {
+
+    Services.prototype.querySpeciesOrigins = jest.fn().mockImplementationOnce(() => {
+      return [];
+    });
+
+    const useCase: SpeciesUseCase = new SpeciesUseCase()
+
+    const expectedResponse: Result = new Result()
+    expectedResponse.content = []
+    expectedResponse.success = new UseCaseSuccess('Query is ok', 200)
+
+    expect(await useCase.getSpeciesOrigins('jwt')).toEqual(expectedResponse)
+  })
+
+  test('getSpeciesOrigins returns error on null', async () => {
+
+    Services.prototype.querySpeciesOrigins = jest.fn().mockImplementationOnce(() => {
+      return new UseCaseError('error', 400);
+    });
+
+    const useCase: SpeciesUseCase = new SpeciesUseCase()
+
+    const expectedResponse: Result = new Result()
+    expectedResponse.errors = [new UseCaseError('error', 400)]
+
+    expect(await useCase.getSpeciesOrigins('jwt')).toEqual(expectedResponse)
+  })
+
+  test('deleteSpecies returns success on Services success', async () => {
+
+    Services.prototype.deleteSpecies = jest.fn().mockImplementationOnce(() => {
+      return true;
+    });
+
+    const useCase: SpeciesUseCase = new SpeciesUseCase()
+
+    const expectedResponse: Result = new Result()
+    expectedResponse.content = {}
+    expectedResponse.success = new UseCaseSuccess('Resource is deleted', 204)
+
+    expect(await useCase.deleteSpecies('jwt', new Species([]))).toEqual(expectedResponse)
+  })
+
+  test('deleteSpecies returns error on null', async () => {
+
+    Services.prototype.deleteSpecies = jest.fn().mockImplementationOnce(() => {
+      return [new UseCaseError('error', 400)];
+    });
+
+    const useCase: SpeciesUseCase = new SpeciesUseCase()
+
+    const expectedResponse: Result = new Result()
+    expectedResponse.errors = [new UseCaseError('error', 400)]
+
+    expect(await useCase.deleteSpecies('jwt', new Species([]))).toEqual(expectedResponse)
+  })
+})
